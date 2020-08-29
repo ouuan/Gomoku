6e7f4db4d396f28b3684033671b5427683f5850d
Yufan You <ouuansteve@gmail.com>
2020-08-29 23:29:52 +0800

    fix: remove unnecessary target

--- not_so_naive.cpp
+++ not_so_naive.cpp
@@ -25,7 +25,6 @@ SOFTWARE.
 */
 
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,popcnt,tune=native")
 
 #include <algorithm>
 #include <chrono>
